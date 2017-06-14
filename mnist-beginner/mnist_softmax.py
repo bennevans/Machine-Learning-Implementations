d730862a0e23b5f715b81fe7fc5a34637d255adf
Benjamin Evans <bevans97@cs.washington.edu>
2017-06-14 14:25:39 -0700

    imported data

---
+++ mnist-beginner/mnist_softmax.py
@@ -0,0 +1,10 @@
+"""
+Implementing a simple softmax regression model on the MNIST data set
+following Google's tensorflow tutorial
+"""
+
+#import the mnist data
+from tensorflow.examples.tutorials.mnis import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+
+
