bd8d29bf4011965587877fb2ed087ed6a28d0827
Jonas Schill <jonas.schill@gmail.com>
2017-06-15 15:41:03 +0200

    hi

--- mnist-beginner/mnist_softmax.py
+++ mnist-beginner/mnist_softmax.py
@@ -3,10 +3,36 @@ Implementing a simple softmax regression model on the MNIST data set
 following Google's tensorflow tutorial
 """
 
-#import the mnist data
-from tensorflow.examples.tutorials.mnis import input_data
+# Import TensorFlow
+import tensorflow as tf
+
+# Import the mnist data
+from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
 
+# Model parameters
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
 
-#HI BEN IS THIS WORKING?
-#Yes
+# Model input and output
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# Loss
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# Optimizer
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# Training
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+for _ in range(1000) :
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# Evaluating the model
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
